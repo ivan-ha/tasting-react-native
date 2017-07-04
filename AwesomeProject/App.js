5fae9ef6ca8d9e69a7cced24f35de791063d9656
Ivan HA <hangolam@gmail.com>
2017-07-04 17:22:08 +0800

    add 2 images

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, Image, View } from 'react-native';
 
-export default class App extends React.Component {
+class MyImage extends Component {
+  render() {
+    let pic = {
+      uri: this.props.uri
+    };
+    return (
+      <Image source={pic} style={styles.image}/>
+    );
+  }
+}
+
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <MyImage uri="https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg" />
+        <MyImage uri="http://blog.iconfinder.com/wp-content/uploads/2014/04/cat_grumpy_orig.png" />
       </View>
     );
   }
@@ -20,4 +33,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: 193,
+    height: 110
+  }
 });
